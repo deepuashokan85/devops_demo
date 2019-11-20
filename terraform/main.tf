92ea703a778232357758620af101482d3c4da5d5
deepuashokan85 <deepuashokan85@gmail.com>
2019-11-21 01:55:45 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,24 +1,26 @@
 provider "aws" {
-  region = var.region
+  region = "us-east-2"
 }
 
 resource "aws_instance" "web" {
-  instance_type = var.instance_type
-  ami           = var.ami
-  count         = var.count
-  key_name      = var.keypair
-  security_groups = [var.sec1_id]
-  subnet_id     = var.subnet_id
-  associate_public_ip_address = var.associate_public_ip_address
+  instance_type = "t2.micro"
+  ami           = "ami-0520e698dd500b1d1"
+  count         = 1
+  key_name      = "devops"
+  security_groups = ["sg-8bd369e5"]
+  subnet_id     = "subnet-21a8df5b"
+  associate_public_ip_address = "true"
 
   tags = {
     Name = "web"
   }
 
 connection {
+    #host = "aws_instance.web.*.public_ip"
     host = aws_instance.web.0.public_ip
-    user = var.username
-    private_key   = file(var.private_key)
+    user = "ec2-user"
+    #private_key   = file("/tmp/devops.pem")
+    private_key   = file(var.private_key_path)
   }
 
   provisioner "file" {
@@ -34,4 +36,3 @@ connection {
     ]
   }
 }
-
